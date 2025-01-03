1ef6ffcb0ec7eef8b0aa7510d5c36565d351fb9d
JowiiProgrammer <jowiigamer07@gmail.com>
2025-01-03 20:22:30 +0100

    first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,9 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
 
+import vercel from '@astrojs/vercel';
+
 export default defineConfig({
   integrations: [tailwind(), react()],
+  adapter: vercel(),
 });
